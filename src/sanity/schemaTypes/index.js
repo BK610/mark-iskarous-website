249c8095a107810d4124f22efde1be4c5254e7d4
BK610 <bailey.orion.kane@gmail.com>
2025-01-16 21:02:11 -0500

    Remove TS features

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,10 +1,10 @@
 // ./src/sanity/schemaTypes/index.ts
-import type { SchemaTypeDefinition } from "sanity";
+// import { SchemaTypeDefinition } from "sanity";
 import { authorType } from "./author";
 import { blockContentType } from "./blockContent";
 import { categoryType } from "./category";
 import { postType } from "./post";
 
-export const schema: { types: SchemaTypeDefinition[] } = {
+export const schema = {
   types: [authorType, blockContentType, categoryType, postType],
 };
