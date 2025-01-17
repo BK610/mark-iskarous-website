8a8d2582b19f30e62595f7134e67385a83396289
BK610 <bailey.orion.kane@gmail.com>
2025-01-16 19:41:18 -0500

    Add sanity import

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sanity from "@sanity/astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), sanity()],
 });
