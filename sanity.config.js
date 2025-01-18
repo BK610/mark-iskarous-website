aa500016c85118edaf5fd0077702c9a00f84414e
BK610 <bailey.orion.kane@gmail.com>
2025-01-17 20:00:52 -0500

    Update all sanity files to match boilerplate

--- sanity.config.js
+++ sanity.config.js
@@ -6,7 +6,5 @@ export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
   plugins: [structureTool()],
-  schema: {
-    types: [schema],
-  },
+  schema,
 }); 
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,10 +1,10 @@
 // ./src/sanity/schemaTypes/index.ts
-// import { SchemaTypeDefinition } from "sanity";
+import type { SchemaTypeDefinition } from "sanity";
 import { authorType } from "./author";
 import { blockContentType } from "./blockContent";
 import { categoryType } from "./category";
 import { postType } from "./post";
 
-export const schema = {
+export const schema: { types: SchemaTypeDefinition[] } = {
   types: [authorType, blockContentType, categoryType, postType],
 };
