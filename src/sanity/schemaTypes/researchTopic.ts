1c66a88db2e84e983fa20d5f28a92cb48be9bf57
BK610 <bailey.orion.kane@gmail.com>
2025-01-27 10:56:26 -0500

    Create researchTopic schema

---
+++ src/sanity/schemaTypes/researchTopic.ts
@@ -0,0 +1,27 @@
+// ./src/sanity/schemaTypes/category.ts
+import { defineField, defineType } from "sanity";
+
+export const researchTopicType = defineType({
+  title: "Research Topic",
+  name: "researchTopic",
+  type: "document",
+  fields: [
+    defineField({
+      title: "Title",
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      title: "Short Description",
+      name: "shortDescription",
+      type: "text",
+      description: "A short description of the research topic. 1-2 lines.",
+    }),
+    defineField({
+      title: "Long Description",
+      name: "longDescription",
+      type: "text",
+      description: "A longer description of the research topic. 1 paragraph.",
+    }),
+  ],
+});
