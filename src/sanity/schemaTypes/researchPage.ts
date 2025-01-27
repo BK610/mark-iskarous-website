776fae576a232ab98aec7d3e6dcf7721b68c28f6
BK610 <bailey.orion.kane@gmail.com>
2025-01-27 14:24:08 -0500

    Create researchPage schema

---
+++ src/sanity/schemaTypes/researchPage.ts
@@ -0,0 +1,28 @@
+import { defineField, defineType } from "sanity";
+
+export const homePageType = defineType({
+  name: "researchPage",
+  title: "Research Page Content",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      description: "Title of this Sanity entry. Not displayed on the website.",
+    }),
+    defineField({
+      name: "heroTitle",
+      title: "Hero Title",
+      type: "string",
+      description: "Title of the hero section.",
+    }),
+    defineField({
+      name: "heroText",
+      title: "Hero Text",
+      type: "array",
+      of: [{ type: "block" }],
+      description: "Description for the hero section.",
+    }),
+  ],
+});
