eb1393c5a00560f9134876b024065ef346b946c8
BK610 <bailey.orion.kane@gmail.com>
2025-01-27 13:48:44 -0500

    Add homePage type, and re-add blockContent type

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,19 +1,21 @@
 // ./src/sanity/schemaTypes/index.ts
 import type { SchemaTypeDefinition } from "sanity";
-import { authorType } from "./author";
+// import { authorType } from "./author";
 import { blockContentType } from "./blockContent";
-import { categoryType } from "./category";
-import { postType } from "./post";
+// import { categoryType } from "./category";
+// import { postType } from "./post";
 import { socialLinkType } from "./socialLink";
 import { researchTopicType } from "./researchTopic";
+import { homePageType } from "./homePage";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     // authorType,
-    // blockContentType,
+    blockContentType,
     // categoryType,
     // postType,
     socialLinkType,
     researchTopicType,
+    homePageType,
   ],
 };
