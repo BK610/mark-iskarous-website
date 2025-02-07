71bc34d8f5ffe73444bed50cc676b64de0a48aee
BK610 <bailey.orion.kane@gmail.com>
2025-02-07 13:46:44 -0500

    Formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,16 +11,20 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), sanity({
-    projectId: 'neusm05o',
-    dataset: 'production',
-    // Set useCdn to false if you're building statically.
-    useCdn: false,
-    studioBasePath: "/admin",
-    apiVersion: "2025-01-16",
-  })],
-  output: 'server',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    sanity({
+      projectId: "neusm05o",
+      dataset: "production",
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+      studioBasePath: "/admin",
+      apiVersion: "2025-01-16",
+    }),
+  ],
+  output: "server",
   adapter: vercel(),
 });
