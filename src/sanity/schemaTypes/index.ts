0361ca539d19c8d622035eba345498158c0eac05
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-22 10:43:42 -0400

    Remove boilerplate schemaTypes

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,5 @@
 // ./src/sanity/schemaTypes/index.ts
 import type { SchemaTypeDefinition } from "sanity";
-// import { authorType } from "./author";
-// import { blockContentType } from "./blockContent";
-// import { postType } from "./post";
 import { socialLinkType } from "./socialLink";
 import { researchTopicType } from "./researchTopic";
 import { homePageType } from "./homePage";
@@ -15,9 +12,6 @@ import { cvType } from "./cv";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    // authorType,
-    // blockContentType,
-    // postType,
     socialLinkType,
     researchTopicType,
     homePageType,
